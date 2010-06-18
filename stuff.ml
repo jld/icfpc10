dd55a2c11690c3532c256bdf5ff4e1282c63b407
Jed Davis <jld@panix.com>
2010-06-18 13:21:34 -0700

    HAHA YES I HAVE THE TRUTH TABLES

--- stuff.ml
+++ stuff.ml
@@ -1,39 +1,37 @@
 open Factory 
 
-let zero_two zero two =
-  let g = gate () in
-  wire zero (L g);
-  wire (L g) (R g);
-  wire (R g) two
+let two_of_zero zero =
+  let p = pipe () in
+  let (zero', two) = fgate zero (P p) in
+  wire zero' (P p);
+  two
 
-let zero_one zero one =
-  let two = P (pipe ()) in
-  zero_two zero two;
-  let g = gate () in
-  wire (R g) (L g);
-  wire two (R g);
-  wire (L g) one
+let one_of_two two =
+  let p = pipe () in
+  let (one, two') = fgate (P p) two in
+  wire two' (P p);
+  one
+
+let one_of_zero zero = one_of_two (two_of_zero zero)
 
 let _ =
-  let p0 = pipe () in
-  let p1 = pipe () in
-  zero_one (P p0) (P p1);
-  let g = gate () in
-  wire X (R g);
-  wire (P p1) (L g);
-  wire (R g) X;
-  wire (L g) (P p0);
+  let p = pipe () in
+  let larg = two_of_zero X 
+  and rarg = one_of_zero (P p) in
+  let (l, r) = fgate larg rarg in
+  wire r X;
+  wire l (P p);
   print ()
   
 
 (* 
 L 0 1 2
 0 0 2 1
-1 1    
-2 2   0
+1 1 0 2
+2 2 1 0
 
 R 0 1 2
 0 2 2 2
-1 2    
-2 2   0
+1 2 0 1
+2 2 1 0
 *)
