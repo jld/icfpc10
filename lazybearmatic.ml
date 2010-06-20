ed016fdc1950fbc039a2e904d0eebdc43849c694
Jed Davis <jld@panix.com>
2010-06-20 13:50:57 -0700

    The lazy bear has gotten more stuff by being run for very long.

--- lazybearmatic.ml
+++ lazybearmatic.ml
@@ -7,9 +7,10 @@ let synth = Fancysynth.synth 4 12 6
 let _ = 
   List.iter (fun (carid, fuel) ->
     factorize_fuel synth carid fuel)
-    (strategize_trusted (lazypunch 6) (targets ()))
+    (strategize_trusted (lazypunch 3) (targets ()))
 (*       (Longpipe.replicate 10 ("5109",(getcar "5109")))) *)
 
 (* 3 -> 10.26s, 2 cars *)
 (* 4 -> 57.41s, 1 car *)
 (* 5 -> 218.74s, 3 cars *)
+(* 6 -> 647.81, 1 car *)
