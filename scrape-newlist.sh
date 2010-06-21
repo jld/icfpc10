ca493b0f4490ce97922e86a864b6079495ddae09
Jed Davis <jld@panix.com>
2010-06-20 17:43:40 -0700

    Scraper for the new-style car list.

---
+++ scrape-newlist.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+LAST=`tail -1 remote/carlist`
+curl 'http://nfa.imn.htwk-leipzig.de/recent_cars/?G0='$LAST \
+    | sed -n 's/^.*(\([0-9]*\),.*,&quot;\([0-2]*\)&quot;).*/\1 \2/p' \
+    | while read carno carstr
+do
+    echo $carno >&2
+    echo $carno >> remote/carlist
+    echo $carstr > remote/cars/$carno
+done
+echo $LAST
