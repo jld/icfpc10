98b6018d1834ef196dbca6312ef413ee3e398437
Jed Davis <jld@panix.com>
2010-06-20 20:33:30 -0700

    Curtail the twobearmatic -- it burns ~all its time on stuff it seems to have not been solving anyway.

--- twobearmatic.ml
+++ twobearmatic.ml
@@ -7,11 +7,6 @@ let synth = Fancysynth.synth 4 12 6
 let _ = 
   List.iter (fun (carid, fuel) ->
     factorize_fuel synth carid fuel)
-    (strategize_trusted (twopunch 8) (List.rev (targets ())))
-(*
-       (List.map (fun (t,n,c) -> n)
-	  (List.sort compare 
-	     (List.map (fun (n,c) -> (tanks c, n, c)) 
-		(targets ())))))
-*)
+    (strategize_trusted (twopunch 8)
+       (List.filter (fun (n,c) -> tanks c <= 5) (targets ())))
  
